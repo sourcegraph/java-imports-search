17b7abc68224c060db61063828174db7157bfa75
Farhan Attamimi <farhan@sourcegraph.com>
2018-10-30 17:03:21 -0700

    Rm space

--- src/java-imports-search.ts
+++ src/java-imports-search.ts
@@ -48,7 +48,7 @@ export function activate(): void {
                                 new sourcegraph.Position(match.lineNumber, 0)
                             ),
                             after: {
-                                contentText: ' See all usages',
+                                contentText: 'See all usages',
                                 linkURL: '/search?q=java.imports:' + match.pkgName,
                                 backgroundColor: 'pink',
                                 color: 'black'

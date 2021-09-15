73c382bb22971f28d0a88d459d2fbff0b65ad08b
TJ Kandala <kandalatj@gmail.com>
2021-09-14 20:56:22 -0400

    refactor: single condition early return

--- src/java-imports-search.ts
+++ src/java-imports-search.ts
@@ -42,49 +42,43 @@ export function activate(
     context.subscriptions.add(
         editorsChanges.subscribe(codeEditors => {
             codeEditors.map(codeEditor => {
-                if (!settings['javaImports.showAllUsagesLinks']) {
-                    return
-                }
-
-                if (codeEditor.type !== 'CodeEditor') {
-                    return
-                }
-
-                const document = codeEditor.document
-                if (document.languageId !== 'java') {
+                if (
+                    !settings['javaImports.showAllUsagesLinks'] ||
+                    codeEditor.type !== 'CodeEditor' ||
+                    codeEditor.document?.languageId !== 'python' ||
+                    !codeEditor.document.text
+                ) {
                     return
                 }
 
                 const matches: { lineNumber: number; pkgName: string }[] = []
-                if (document.text) {
-                    const lines = document.text.split('\n')
-                    lines.map((line, lineNumber) => {
-                        const javaPkgRegex = /^import\s(?:static\s)?([^\s]*)[^\s]*;$/
-                        const match = javaPkgRegex.exec(line)
-                        if (match && match.length > 1) {
-                            // The match index depends on which regex pattern actually produced a match
-                            const pkgName = match[1]
-                            matches.push({ lineNumber, pkgName })
-                        }
-                    })
-
-                    if (matches.length > 0) {
-                        codeEditor.setDecorations(
-                            decorationType,
-                            matches.map(match => ({
-                                range: new sourcegraph.Range(
-                                    new sourcegraph.Position(match.lineNumber, 0),
-                                    new sourcegraph.Position(match.lineNumber, 0)
-                                ),
-                                after: {
-                                    contentText: 'See all usages',
-                                    linkURL: '/search?q=java.imports:' + match.pkgName + '&patternType=regexp',
-                                    backgroundColor: 'pink',
-                                    color: 'black',
-                                },
-                            }))
-                        )
+                const lines = codeEditor.document.text.split('\n')
+                lines.map((line, lineNumber) => {
+                    const javaPkgRegex = /^import\s(?:static\s)?([^\s]*)[^\s]*;$/
+                    const match = javaPkgRegex.exec(line)
+                    if (match && match.length > 1) {
+                        // The match index depends on which regex pattern actually produced a match
+                        const pkgName = match[1]
+                        matches.push({ lineNumber, pkgName })
                     }
+                })
+
+                if (matches.length > 0) {
+                    codeEditor.setDecorations(
+                        decorationType,
+                        matches.map(match => ({
+                            range: new sourcegraph.Range(
+                                new sourcegraph.Position(match.lineNumber, 0),
+                                new sourcegraph.Position(match.lineNumber, 0)
+                            ),
+                            after: {
+                                contentText: 'See all usages',
+                                linkURL: '/search?q=java.imports:' + match.pkgName + '&patternType=regexp',
+                                backgroundColor: 'pink',
+                                color: 'black',
+                            },
+                        }))
+                    )
                 }
             })
         })
